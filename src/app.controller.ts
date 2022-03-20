e2335369f6648cf1eb5e819c08c14f3f430ad92b
zidanesc <zidankren02@gmail.com>
2022-03-20 22:59:12 +0700

    Learn NestJs V8 (API Versioning)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,36 @@ import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from './user.entity';
 
-@Controller()
+@Controller({
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   async getHello(): Promise<any> {
+    // await this.appService.seed();
+    // return 'seed complete';
+
+    return this.appService.getEmployeeById(2);
+
+    // return this.appService.deleteEmployee(1);
+  }
+
+  @Version('2')
+  @Get()
+  async getHello2(): Promise<any> {
+    // await this.appService.seed();
+    // return 'seed complete';
+
+    return this.appService.getEmployeeById(2);
+
+    // return this.appService.deleteEmployee(1);
+  }
+
+  @Version(VERSION_NEUTRAL)
+  @Get()
+  async getHello3(): Promise<any> {
     // await this.appService.seed();
     // return 'seed complete';
 
